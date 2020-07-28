74dcb574083277234080f5d6b520bd88a55371c4
swift502 <blaha.j502@gmail.com>
2020-07-28 22:43:55 +0200

    nesting v1

--- santacodes-region-viewer/src/regionTreeDataProvider.ts
+++ santacodes-region-viewer/src/regionTreeDataProvider.ts
@@ -19,8 +19,13 @@ export class RegionTreeDataProvider implements vscode.TreeDataProvider<Dependenc
 		return element;
 	}
 
-	getChildren(element?: Dependency): Thenable<Dependency[]> {
-		return Promise.resolve(this.data!);
+	getChildren(element?: Dependency): vscode.ProviderResult<Dependency[]> {
+		
+		if (element === undefined) {
+			return this.data;
+		}
+
+		return element.children;
 	}
 
 	private findRegion(): void {
@@ -29,6 +34,8 @@ export class RegionTreeDataProvider implements vscode.TreeDataProvider<Dependenc
 			return
 
 		let treeRoot = [];
+		let regionStack = [];
+
 		for (let i = 0; i < document.lineCount; i++) {
 			const line = document.lineAt(i);
 			if (line == undefined)
@@ -41,7 +48,7 @@ export class RegionTreeDataProvider implements vscode.TreeDataProvider<Dependenc
 
 			// Region markers as they're documented in:
 			// https://code.visualstudio.com/updates/v1_17#_folding-regions
-			const regionMarkers = [
+			const startMarkers = [
 				"//#region",
 				"//region",
 				"#region",
@@ -49,24 +56,68 @@ export class RegionTreeDataProvider implements vscode.TreeDataProvider<Dependenc
 				"#Region"
 			]
 
-			for (const marker of regionMarkers) {
-				// Check if line starts with on of the region markers
-				if(text.trim().startsWith(marker)) {
-					let name = text.replace(marker, "").trim(); // remove first marker
-					if (name.length === 0) name = "region"; // ensure name is not empty
-					name = "# " + name; // prepend with the # symbol
+			const endMarkers = [
+				"//#endregion",
+				"//endregion",
+				"#endregion",
+				"#pragma endregion",
+				"#End Region"
+			]
 
-					treeRoot.push(new Dependency(name, i, vscode.TreeItemCollapsibleState.None));
+			// Region start
+			for (const marker of startMarkers) {
+				if(text.trim().startsWith(marker)) {
+					const name = this.getRegionName(text, marker);
+					const dep = new Dependency(name, i);
+
+					if (regionStack.length > 0) {
+						let parentDep = regionStack[regionStack.length - 1];
+						parentDep.children?.push(dep);
+						parentDep.collapsibleState =  vscode.TreeItemCollapsibleState.Collapsed;	// Possibly optimise
+					}
+
+					regionStack.push(dep);
+				}
+			}
+
+			// Region end
+			for (const marker of endMarkers) {
+				if(text.trim().startsWith(marker)) {
+					if (regionStack.length === 1) {
+						treeRoot.push(regionStack[0]);
+					}
+
+					regionStack.pop();
 				}
 			}
 		}
+
+		// Resolve remaining stack entries
+		// These regions were opened but never closed
+		if (regionStack.length > 0) {
+			treeRoot.push(regionStack[0]);
+		}
+
 		this.data = treeRoot;
+	}
+
+	private getRegionName(line: string, marker: string): string {
+		// remove first marker
+		let name = line.replace(marker, "").trim();
+		// ensure name is not empty
+		if (name.length === 0) name = "region";
+		// prepend with the # symbol
+		return "# " + name;
 	}
 }
 
 class Dependency extends vscode.TreeItem {
-	constructor(label: string, line: number, collapsibleState: vscode.TreeItemCollapsibleState) {
-		super(label, collapsibleState);
+
+	children: Dependency[] = [];
+
+	constructor(label: string, line: number) {
+		super(label, vscode.TreeItemCollapsibleState.None);
+
 		this.command = {
 			title: '',
 			command: 'region-viewer.reveal',
